3dedc76625e040d88535fad38564fffb1f05e86d
Katerina Zhadko <katerinkaj@icloud.com>
2023-05-09 01:14:08 +0300

    refact: add absolute path

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,11 @@
 import * as fs from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-let readStream = fs.createReadStream('01-read-file\\text.txt', 'utf8');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const absolutePath = path.join(__dirname, 'text.txt');
+let readStream = fs.createReadStream(absolutePath, 'utf8');
 
 readStream.on('data', (chunk)=>{
   console.log(chunk);
