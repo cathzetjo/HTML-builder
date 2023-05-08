69d0dd4b1603383551c84812d56012691484d9e8
Katerina Zhadko <katerinkaj@icloud.com>
2023-05-09 02:19:28 +0300

    feat: add copy of directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+import * as fs from 'node:fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const absolutePathBase = path.join(__dirname, 'files');
+const absolutePathCopy = path.join(__dirname, 'files-copy');
+
+async function copyFiles(base, copy) {
+  await fs.promises.rm(copy, { recursive: true, force: true });
+  fs.mkdir(copy, { recursive: true }, (error) => {
+    if (error) throw error;
+  });
+  fs.readdir(base, {withFileTypes: true}, (error, files) => {
+    if (error) throw error;
+    files.forEach(file => {
+      if(file.isFile()) {
+        fs.copyFile(path.join(base, file.name), path.join(copy, file.name), error => {
+          if (error) throw error;
+        });
+      }
+      if(file.isDirectory()) {
+        fs.mkdir(path.join(copy, file.name), { recursive: true }, (error) => {
+          if (error) throw error;
+        });
+        copyFiles(path.join(base, file.name), path.join(copy, file.name));
+      }
+    });
+  });
+}
+
+copyFiles(absolutePathBase, absolutePathCopy);
